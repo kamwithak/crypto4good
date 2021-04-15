3ea1c0ea2f45ed3b2f69f1d2e9b7dcd005e283f7
an0n <theblizzzzard@gmail.com>
2021-04-15 19:57:46 -0400

    NEW: build init

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,65 +1,80 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import logo from './logo.svg';
+import Typist from 'react-text-typist';
 import './App.css';
 
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    };
+  }
+
+  componentDidMount() {
+    this.getTimeDifference(this.props.eventDate);
+    setInterval(() => this.getTimeDifference(this.props.eventDate), 1000);
+  }
+
+  leadingZero(num) {
+    return (num < 10 && num > 0) ? "0" + num : num;
+  }
+
+  getTimeDifference(eventDate) {
+    const time = Date.parse(eventDate) - Date.parse(new Date());
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((time / 1000 / 60) % 60);
+    const seconds = Math.floor((time / 1000) % 60);
+    this.setState({ days, hours, minutes, seconds });
+  }
+
+  render() {
+    return (
+      <div>
+        <br />
+        <div className="clock">
+        🎉    {this.leadingZero(this.state.days)} {this.state.days == 1 ? 'Day' : 'Days'}
+        </div>
+        <div className="clock">
+          {this.leadingZero(this.state.hours)} {this.state.hours == 1 ? 'Hour' : 'Hours'}
+        </div>
+        <div className="clock">
+          {this.leadingZero(this.state.minutes)} {this.state.minutes == 1 ? 'Minute' : 'Minutes'}
+        </div>
+        <div className="clock">
+          {this.leadingZero(this.state.seconds)} {this.state.seconds == 1 ? 'Second' : 'Seconds'}    🎉
+        </div>
+      </div>
+    );
+  }
+}
 function App() {
   const [message, setMessage] = useState(null);
   const [isFetching, setIsFetching] = useState(false);
   const [url, setUrl] = useState('/api');
 
-  const fetchData = useCallback(() => {
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        setMessage(json.message);
-        setIsFetching(false);
-      }).catch(e => {
-        setMessage(`API call failed: ${e}`);
-        setIsFetching(false);
-      })
-  }, [url]);
-
-  useEffect(() => {
-    setIsFetching(true);
-    fetchData();
-  }, [fetchData]);
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        { process.env.NODE_ENV === 'production' ?
-            <p>
-              This is a production build from create-react-app.
-            </p>
-          : <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-        }
-        <p>{'« '}<strong>
-          {isFetching
-            ? 'Fetching message from API'
-            : message}
-        </strong>{' »'}</p>
-        <p><a
-          className="App-link"
-          href="https://github.com/mars/heroku-cra-node"
-        >
-          React + Node deployment on Heroku
-        </a></p>
-        <p><a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a></p>
+        <br />
+        <Typist
+          sentences={[
+            'Interested in making a donation? 💸',
+            'Be apart of NFT donation history! 📜' ,
+            '100% proceeds go to the Red-Cross Organization! 🎗️',
+            'Welcome to CryptosBiggestWhale.com 🐋']}
+          loop={!true}
+          typingSpeed={25}
+          deletingSpeed={25}
+          cursorColor='white'
+        />
+        <Timer key={'April 30, 2021'} eventName={'Auction End'} eventDate={'April 30, 2021'} />
+        <br />
+        <br />
+        <nft-card contractAddress="0x12f28e2106ce8fd8464885b80ea865e98b465149" tokenId="100030016"> </nft-card>
       </header>
     </div>
   );
